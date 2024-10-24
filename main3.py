8a96f295bf4296b5d40e666ef3bc0066b33a06d0
TSerranoE <156811950+TSerranoE@users.noreply.github.com>
2024-10-23 22:44:40 -0300

    versión 2

--- main3.py
+++ main3.py
@@ -1,46 +1,55 @@
 import numpy as np
 from scipy.optimize import minimize
+import timeout_decorator
 
 #Constantes
-T = 5
-N = 100
+T = 4
+N = 50
 vf = 4
 lambda_value = 0.01
 sigma = 1
 gamma = 1
 
 # Variables iniciales
-iniciales = [3, 2, 1] # x0, v0, alpha0
+iniciales = [3, 4, 1] # x0, v0, alpha0
 vars0 = np.zeros((N, 3))
 vars0[0] = iniciales
 
 # Transformar las 3 columnas en un vector
 vars0 = vars0.T.flatten()
 
-# Control
-u = np.zeros(N)
 
 # Parámetros
 parametros = [T, N, vf, lambda_value, sigma, gamma]
 
 # Definición de la función objetivo
-def obj_funct(vars, parametros):
-    _, N, vf, lambda_value, _, _ = parametros
+def obj_funct(vars, kernel, parametros):
+    T, N, vf, lambda_value, _, gamma = parametros
     x = vars[:2*N].reshape(2, N).T
     alpha = vars[2*N:]
-    return (x[N-1][1]-vf)**2 + lambda_value*np.sum(alpha**2)
+
+    x_gorro = np.zeros((N, 2))
+    x_gorro[0] = iniciales[:2]
+    # Restricciones sobre x
+    for i in range(N-1):
+        x_gorro[i+1][0] =  x_gorro[i][0] + (T/N)*(x_gorro[i][1]) 
+        x_gorro[i+1][1] = x_gorro[i][1] + (T/N)*(-gamma*x_gorro[i][0] + x_gorro[i][1] + sum(alpha[l]*
+                                                       kernel(x, x_gorro, l, i, parametros) for l in range(N))) 
+    print("valor", (x_gorro[N-1][1]-vf)**2 + lambda_value*np.sum(alpha**2))
+    return (x_gorro[N-1][1]-vf)**2 + lambda_value*np.sum(alpha**2)
 
 # Definición del kernel
-def kernel(x, l, i, parametros):
+def kernel(x, x_gorro, l, i, parametros):
     _, _, _, _, sigma, _ = parametros
-    return np.exp(-(1/(2*sigma**2))  *  ((x[l][0]-x[i][0])**2 + (x[l][1]-x[i][1])**2))
+    return np.exp(-(1/(2*sigma**2))  *  ((x[l][0]-x_gorro[i][0])**2 + (x[l][1]-x_gorro[i][1])**2))
 
 # Restricciones sobre x
-def f_rest(vars, u, i, parametros):
+def f_rest(vars, u, i, x_gorro,parametros):
     T, N, _, _, _ , gamma= parametros
     x = vars[:2*N].reshape(2, N).T
     alpha = vars[2*N:]
-    return [ x[i+1][0] -  x[i][0] + (T/N)*(x[i][1]) , x[i+1][1] - x[i][1] + (T/N)*(-gamma*x[i][0] + x[i][1] + u[i]) ]
+
+
 
 # Restricciones sobre u
 def u_rest(vars, u, i, kernel, parametros):
@@ -50,24 +59,21 @@ def u_rest(vars, u, i, kernel, parametros):
     return u[i] - sum(alpha[l]*kernel(x, l, i, parametros) for l in range(N))
 
 # Definición de la función de optimización
-def OptimalControlbyLSSVM(vars0, obj_funct, f_rest, u_rest, kernel, parametros):
-    constraints = []
+@timeout_decorator.timeout(300) 
+def OptimalControlbyLSSVM(vars0, obj_funct, kernel, parametros):
 
-    # Restricciones sobre x
-    for i in range(N-1):
-        constraints.append({'type': 'eq', 'fun': lambda vars: f_rest(vars, u, i, parametros)})
-    
-    # Restricciones sobre u
-    for i in range(N-1):
-        constraints.append({'type': 'eq', 'fun': lambda vars: u_rest(vars, u, i, kernel, parametros)})
-
-    
-    sol = minimize(lambda vars: obj_funct(vars, parametros), vars0, method='SLSQP', constraints=constraints)
+    sol = minimize(lambda vars: obj_funct(vars, kernel,parametros), vars0, method='SLSQP')
     return sol
 
 # Ejecución de la optimización
-result = OptimalControlbyLSSVM(vars0, obj_funct, f_rest, u_rest, kernel, parametros)
-print(result)
+
+try:
+    result = OptimalControlbyLSSVM(vars0, obj_funct, kernel, parametros)
+    print(result)
+except timeout_decorator.timeout_decorator.TimeoutError:
+    print("La optimización se detuvo después de 60 segundos.")
+
+
 import matplotlib.pyplot as plt
 
 # Extraer las soluciones
