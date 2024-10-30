6f4be8831827c2888abba871dcacd86b5b69aec2
TSerranoE <156811950+TSerranoE@users.noreply.github.com>
2024-10-30 01:14:01 -0300

    Casi listo tercer ejemplo

--- Ej3_PenduloInvertido.py
+++ Ej3_PenduloInvertido.py
@@ -1,87 +1,118 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import control as ct
+
 
 # Definición de la función objetivo
 def funcion_objetivo(vars, kernel, valores_iniciales, parametros):
-    T, N, lambda_value, sigma, h, m, m_t, l, g, Q, R, A , B = parametros
+    T, N, lambda_value, sigma, h, m, m_t, largo, g, Q, R, A , B = parametros
+
+    # LQR
+    K, S, E = ct.lqr(A, B, Q, R)
 
     # Extraer las variables
-    x = vars[:2*N].reshape(2, N).T
-    alpha = vars[2*N:]
+    x = vars[:4*N].reshape(4, N).T
+    alpha = vars[4*N:]
 
     # Creación de x_gorro
-    x_gorro = np.zeros((N, 2))
-    x_gorro[0] = valores_iniciales[:2]
+    x_gorro = np.zeros((N, 4))
+    x_gorro[0] = valores_iniciales[:4]
+    u = np.zeros((N, 4))
     for i in range(N-1):
-        x_gorro[i+1][0] =  x_gorro[i][0] + (T/N)*(x_gorro[i][1]) 
-        x_gorro[i+1][1] = x_gorro[i][1] + (T/N)*(-gamma*x_gorro[i][0] + x_gorro[i][1] + sum(alpha[l]*
-                                                       kernel(x, x_gorro, l, i, parametros) for l in range(N))) 
+        k1 = A @ x_gorro[i] + B @ u[i]
+        k2 = A @ (x_gorro[i] + (h/2)*k1) + B @ (u[i] + (h/2)*k1)
+        k3 = A @ (x_gorro[i] + (h/2)*k2) + B @ (u[i] + (h/2)*k2)
+        k4 = A @ (x_gorro[i] + h*k3) + B @ (u[i] + h*k3)
+
+        u[i] = np.array([sum(alpha[l] * kernel(x, x_gorro, l, i, j, parametros) for l in range(N)) for j in range(4)])
+        x_gorro[i+1] = x_gorro[i] + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
+    u[N-1] = np.array([sum(alpha[l] * kernel(x, x_gorro, l, N-1, j, parametros) for l in range(N)) for j in range(4)])
     
     # Imprimir el valor de la función objetivo
-    print("valor", (x_gorro[N-1][1]-vf)**2 + lambda_value*np.sum(alpha**2))
-    return (x_gorro[N-1][1]-vf)**2 + lambda_value*np.sum(alpha**2)
+    objetivo = sum(x_gorro[i].T @ Q @ x_gorro[i] + R*u[i].T @ u[i] + lambda_value * alpha[i]**2 for i in range(N))
+    print("valor" , objetivo )
+    return (objetivo)
 
 # Definición variables valores_iniciales
 def variable_inicial(valores_iniciales, parametros):
-    T, N, lambda_value, sigma, h, m, m_t, l, g, Q, R, A , B = parametros
-    variable_inicial = np.zeros((N, 3))
+    T, N, lambda_value, sigma, h, m, m_t, largo, g, Q, R, A , B = parametros
+    variable_inicial = np.zeros((N, 5))
     variable_inicial[0] = valores_iniciales
     return variable_inicial.T.flatten()
 
 # Definición del kernel en este caso RBF
-def kernel(x, x_gorro, l, i, parametros):
-    T, N, lambda_value, sigma, h, m, m_t, l, g, Q, R, A , B = parametros
-    return np.exp(-(1/(2*sigma**2))  *  ((x[l][0]-x_gorro[i][0])**2 + (x[l][1]-x_gorro[i][1])**2))
+def kernel(x, x_gorro, l, i, index, parametros):
+    T, N, lambda_value, sigma, h, m, m_t, largo, g, Q, R, A , B = parametros
+    return np.exp(-(1/(2*sigma**2))  *  ((x[l][index]-x_gorro[i][index])**2)) 
 
 # Graficar las soluciones
 def plot(result, valores_iniciales, parametros):
-    T, N, lambda_value, sigma, h, m, m_t, l, g, Q, R, A , B = parametros
+    T, N, lambda_value, sigma, h, m, m_t, largo, g, Q, R, A , B = parametros
+
 
     # Extraer las soluciones
-    x_sol = result.x[:2*N].reshape(2, N).T
-    alpha_sol = result.x[2*N:]
+    x_sol = result.x[:4*N].reshape(4, N).T
+    alpha_sol = result.x[4*N:]
 
-    # creación de x_gorro
-    x_gorro = np.zeros((N, 2))
-    x_gorro[0] = valores_iniciales[:2]
 
-    # Definición del control
-    u_opt = np.zeros(N)
-
+    x_gorro = np.zeros((N, 4))
+    x_gorro[0] = valores_iniciales[:4]
+    u_opt = np.zeros((N, 4))
     for i in range(N-1):
-        x_gorro[i+1][0] =  x_gorro[i][0] + (T/N)*(x_gorro[i][1]) 
-        x_gorro[i+1][1] = x_gorro[i][1] + (T/N)*(-gamma*x_gorro[i][0] + x_gorro[i][1] + sum(alpha_sol[l]*
-                                                        kernel(x_sol, x_gorro, l, i, parametros) for l in range(N))) 
+        k1 = A @ x_gorro[i] + B @ u_opt[i]
+        k2 = A @ (x_gorro[i] + (h/2)*k1) + B @ (u_opt[i] + (h/2)*k1)
+        k3 = A @ (x_gorro[i] + (h/2)*k2) + B @ (u_opt[i] + (h/2)*k2)
+        k4 = A @ (x_gorro[i] + h*k3) + B @ (u_opt[i] + h*k3)
+
+        u_opt[i] = np.array([sum(alpha_sol[l] * kernel(x_sol, x_gorro, l, i, j, parametros) for l in range(N)) for j in range(4)])
+        x_gorro[i+1] = x_gorro[i] + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
+    u_opt[N-1] = np.array([sum(alpha_sol[l] * kernel(x_sol, x_gorro, l, N-1, j, parametros) for l in range(N)) for j in range(4)])
     
-    for i in range(N):
-       u_opt[i] = sum(alpha_sol[l]* kernel(x_sol, x_gorro, l, i, parametros) for l in range(N))
+    # Imprimir el valor de la función objetivo
+    objetivo = sum(x_gorro[i].T @ Q @ x_gorro[i] + R*u_opt[i].T @ u_opt[i] + lambda_value * alpha_sol[i]**2 for i in range(N))   
 
     # Imprimir el valor de la función objetivo    
-    print("valor", (x_gorro[N-1][1]-vf)**2 + lambda_value*np.sum(alpha_sol**2))
-    print("velocidad final", x_gorro[N-1][1])
+    print("valor", objetivo)
+    print("posición final", x_gorro[N-1])
 
     # Graficar las soluciones
-    plt.figure(figsize=(12, 9))
+    plt.figure(figsize=(12, 12))
 
     # Posición x
-    plt.subplot(3, 1, 1)
-    plt.plot(np.linspace(0,T,N), x_gorro[:, 0], label='Posición x')
+    plt.subplot(5, 1, 1)
+    plt.plot(np.linspace(0, T, N), x_gorro[:, 0], label='Posición x')
     plt.xlabel('Tiempo')
     plt.ylabel('Posición x')
     plt.legend()
     plt.grid()
 
     # Velocidad v
-    plt.subplot(3, 1, 2)
-    plt.plot(np.linspace(0,T,N), x_gorro[:, 1], label='Velocidad v', color='orange')
+    plt.subplot(5, 1, 2)
+    plt.plot(np.linspace(0, T, N), x_gorro[:, 1], label='Velocidad v', color='orange')
     plt.xlabel('Tiempo')
     plt.ylabel('Velocidad v')
     plt.legend()
     plt.grid()
 
+    # Angulo theta
+    plt.subplot(5, 1, 3)
+    plt.plot(np.linspace(0, T, N), x_gorro[:, 2], label='Angulo theta')
+    plt.xlabel('Tiempo')
+    plt.ylabel('Angulo theta')
+    plt.legend()
+    plt.grid()
+
+    # Velocidad angular w
+    plt.subplot(5, 1, 4)
+    plt.plot(np.linspace(0, T, N), x_gorro[:, 3], label='Velocidad angular w')
+    plt.xlabel('Tiempo')
+    plt.ylabel('Velocidad angular w')
+    plt.legend()
+    plt.grid()
+
     # Control
-    plt.subplot(3, 1, 3)
-    plt.plot(np.linspace(0,T,N), u_opt, label='Control Óptimo')
+    plt.subplot(5, 1, 5)
+    plt.plot(np.linspace(0, T, N), u_opt, label='Control Óptimo')
     plt.xlabel('Tiempo')
     plt.ylabel('Control')
     plt.legend()
--- solucionador_PenduloInvertido.py
+++ solucionador_PenduloInvertido.py
@@ -11,22 +11,19 @@ sigma = 1
 h = 0.05
 m = 0.1
 m_t = 1.1
-l = 0.5
+largo = 0.5
 g = 9.81
 Q = np.eye(4)
 R = 0.01
-A = np.zeros((4,4))
-B = np.zeros(4)
-
-print(A)
-print(B)
+A = np.array([[0, 1, 0, 0], [0, 0, -g*m/(4/3*m_t-m), 0], [0, 0, 0, 1], [0, 0, m_t*g/(largo*(4/3*m_t-m)), 0]])
+B = np.array([0, 4/3*(1/(4/3*m_t-m)), 0, -1/(largo*(4/3*m_t-m))]).T
 
 
 # Parámetros
-parametros = [T, N, lambda_value, sigma, h, m, m_t, l, g, Q, R, A , B]
+parametros = [T, N, lambda_value, sigma, h, m, m_t, largo, g, Q, R, A , B]
 
 # x0, v0, theta0, w0, alpha0
-valores_iniciales = [0, 0 , 0, 0, 0.1] 
+valores_iniciales = [0, 0 , np.pi, 0, 0.1] 
 
 variable_inicial = variable_inicial(valores_iniciales, parametros)
 
